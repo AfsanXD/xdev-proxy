4ea373d138fecb21208370525a3c41e310cb5287
AfsanXD <72094019+AfsanXD@users.noreply.github.com>
2025-06-13 07:04:47 +0600

    v4

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  // Increase the response size limit for the proxy
+  experimental: {
+    largePageDataBytes: 128 * 1024 * 1024, // 128MB
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +30,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
